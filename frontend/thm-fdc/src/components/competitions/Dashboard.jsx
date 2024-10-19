c352d4756644748ce2ab007f4c5751794b41076c
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-10-19 16:52:54 -0300

    feat: start competitions page on frontend

---
+++ frontend/thm-fdc/src/components/competitions/Dashboard.jsx
@@ -0,0 +1,226 @@
+// material-ui
+import Avatar from '@mui/material/Avatar';
+import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
+import ListItemText from '@mui/material/ListItemText';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+// project import
+import MainCard from '../general/components/MainCard';
+
+// assets
+import GiftOutlined from '@ant-design/icons/GiftOutlined';
+import MessageOutlined from '@ant-design/icons/MessageOutlined';
+import SettingOutlined from '@ant-design/icons/SettingOutlined';
+
+// avatar style
+const avatarSX = {
+  width: 36,
+  height: 36,
+  fontSize: '1rem'
+};
+
+// action style
+const actionSX = {
+  mt: 0.75,
+  ml: 1,
+  top: 'auto',
+  right: 'auto',
+  alignSelf: 'flex-start',
+  transform: 'none'
+};
+
+// ==============================|| DASHBOARD - DEFAULT ||============================== //
+
+export default function Dashboard() {
+  return (
+    <Grid container rowSpacing={4.5} columnSpacing={2.75} sx={{ marginTop: 4, bgcolor: "#fafafa", justifyContent: "center" }}>
+      {/* row 4 */}
+      <Grid item xs={12} md={5} lg={10} sx={{ marginTop: 4, marginBottom: 6 }}>
+        <Grid container alignItems="center" justifyContent="center">
+          <Grid item z>
+            <Typography variant="h5">Campeonatos</Typography>
+          </Grid>
+          <Grid item />
+        </Grid>
+        <MainCard sx={{ mt: 2 }} content={false}>
+          <List
+            component="nav"
+            sx={{
+              px: 0,
+              py: 0,
+              '& .MuiListItemButton-root': {
+                py: 1.5,
+                '& .MuiAvatar-root': avatarSX,
+                '& .MuiListItemSecondaryAction-root': { ...actionSX, position: 'relative' }
+              }
+            }}
+          >
+            <ListItemButton divider>
+              <ListItemAvatar>
+                <Avatar sx={{ color: 'success.main', bgcolor: 'success.lighter' }}>
+                  <GiftOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={<Typography variant="subtitle1">Order #002434</Typography>} secondary="Today, 2:00 AM" />
+              <ListItemSecondaryAction>
+                <Stack alignItems="flex-end">
+                  <Typography variant="subtitle1" noWrap>
+                    + $1,430
+                  </Typography>
+                  <Typography variant="h6" color="secondary" noWrap>
+                    78%
+                  </Typography>
+                </Stack>
+              </ListItemSecondaryAction>
+            </ListItemButton>
+            <ListItemButton divider>
+              <ListItemAvatar>
+                <Avatar sx={{ color: 'primary.main', bgcolor: 'primary.lighter' }}>
+                  <MessageOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={<Typography variant="subtitle1">Order #984947</Typography>} secondary="5 August, 1:45 PM" />
+              <ListItemSecondaryAction>
+                <Stack alignItems="flex-end">
+                  <Typography variant="subtitle1" noWrap>
+                    + $302
+                  </Typography>
+                  <Typography variant="h6" color="secondary" noWrap>
+                    8%
+                  </Typography>
+                </Stack>
+              </ListItemSecondaryAction>
+            </ListItemButton>
+            <ListItemButton divider>
+              <ListItemAvatar>
+                <Avatar sx={{ color: 'error.main', bgcolor: 'error.lighter' }}>
+                  <SettingOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
+              <ListItemSecondaryAction>
+                <Stack alignItems="flex-end">
+                  <Typography variant="subtitle1" noWrap>
+                    + $682
+                  </Typography>
+                  <Typography variant="h6" color="secondary" noWrap>
+                    16%
+                  </Typography>
+                </Stack>
+              </ListItemSecondaryAction>
+            </ListItemButton>
+            <ListItemButton divider>
+              <ListItemAvatar>
+                <Avatar sx={{ color: 'error.main', bgcolor: 'error.lighter' }}>
+                  <SettingOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
+              <ListItemSecondaryAction>
+                <Stack alignItems="flex-end">
+                  <Typography variant="subtitle1" noWrap>
+                    + $682
+                  </Typography>
+                  <Typography variant="h6" color="secondary" noWrap>
+                    16%
+                  </Typography>
+                </Stack>
+              </ListItemSecondaryAction>
+            </ListItemButton>
+            <ListItemButton divider>
+              <ListItemAvatar>
+                <Avatar sx={{ color: 'error.main', bgcolor: 'error.lighter' }}>
+                  <SettingOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
+              <ListItemSecondaryAction>
+                <Stack alignItems="flex-end">
+                  <Typography variant="subtitle1" noWrap>
+                    + $682
+                  </Typography>
+                  <Typography variant="h6" color="secondary" noWrap>
+                    16%
+                  </Typography>
+                </Stack>
+              </ListItemSecondaryAction>
+            </ListItemButton><ListItemButton divider>
+              <ListItemAvatar>
+                <Avatar sx={{ color: 'error.main', bgcolor: 'error.lighter' }}>
+                  <SettingOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
+              <ListItemSecondaryAction>
+                <Stack alignItems="flex-end">
+                  <Typography variant="subtitle1" noWrap>
+                    + $682
+                  </Typography>
+                  <Typography variant="h6" color="secondary" noWrap>
+                    16%
+                  </Typography>
+                </Stack>
+              </ListItemSecondaryAction>
+            </ListItemButton><ListItemButton divider>
+              <ListItemAvatar>
+                <Avatar sx={{ color: 'error.main', bgcolor: 'error.lighter' }}>
+                  <SettingOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
+              <ListItemSecondaryAction>
+                <Stack alignItems="flex-end">
+                  <Typography variant="subtitle1" noWrap>
+                    + $682
+                  </Typography>
+                  <Typography variant="h6" color="secondary" noWrap>
+                    16%
+                  </Typography>
+                </Stack>
+              </ListItemSecondaryAction>
+            </ListItemButton><ListItemButton divider>
+              <ListItemAvatar>
+                <Avatar sx={{ color: 'error.main', bgcolor: 'error.lighter' }}>
+                  <SettingOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
+              <ListItemSecondaryAction>
+                <Stack alignItems="flex-end">
+                  <Typography variant="subtitle1" noWrap>
+                    + $682
+                  </Typography>
+                  <Typography variant="h6" color="secondary" noWrap>
+                    16%
+                  </Typography>
+                </Stack>
+              </ListItemSecondaryAction>
+            </ListItemButton><ListItemButton divider>
+              <ListItemAvatar>
+                <Avatar sx={{ color: 'error.main', bgcolor: 'error.lighter' }}>
+                  <SettingOutlined />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
+              <ListItemSecondaryAction>
+                <Stack alignItems="flex-end">
+                  <Typography variant="subtitle1" noWrap>
+                    + $682
+                  </Typography>
+                  <Typography variant="h6" color="secondary" noWrap>
+                    16%
+                  </Typography>
+                </Stack>
+              </ListItemSecondaryAction>
+            </ListItemButton>
+          </List>
+        </MainCard>
+      </Grid>
+    </Grid>
+  );
+}

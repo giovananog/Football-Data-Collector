f768707affb461b8630243c602be35b6892fa904
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-10-19 17:55:04 -0300

    feat: add new statistics ection on general page

--- frontend/thm-fdc/src/components/general/components/Dashboard.jsx
+++ frontend/thm-fdc/src/components/general/components/Dashboard.jsx
@@ -1,12 +1,8 @@
 // material-ui
-import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
 import ListItemText from '@mui/material/ListItemText';
-import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import ForwardOutlinedIcon from '@mui/icons-material/ForwardOutlined';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
@@ -17,27 +13,6 @@ import AnalyticEcommerce from './AnalyticEcommerce';
 import ReportAreaChart from './ReportAreaChart';
 import OrdersTable from './OrdersTable';
 
-// assets
-import GiftOutlined from '@ant-design/icons/GiftOutlined';
-import MessageOutlined from '@ant-design/icons/MessageOutlined';
-import SettingOutlined from '@ant-design/icons/SettingOutlined';
-
-// avatar style
-const avatarSX = {
-  width: 36,
-  height: 36,
-  fontSize: '1rem'
-};
-
-// action style
-const actionSX = {
-  mt: 0.75,
-  ml: 1,
-  top: 'auto',
-  right: 'auto',
-  alignSelf: 'flex-start',
-  transform: 'none'
-};
 
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
@@ -45,8 +20,9 @@ export default function Dashboard() {
   return (
     <Grid container rowSpacing={4.5} columnSpacing={2.75} sx={{ marginTop: 4, bgcolor: "#fafafa", justifyContent: "center" }}>
       {/* row 1 */}
-      <Grid item xs={12} lg={12} sx={{ mb: -2.25 }} alignItems="center" justifyContent='space-between' direction="column">
-        <Typography variant="h5" sx={{ marginLeft: 10 }}>Dashboard</Typography>
+      <Grid item xs={12} lg={12} sx={{ mb: -2.25 }} textAlign={"center"}>
+        <Typography variant="h5">Nossos dados</Typography>
+        <Typography variant="p">Nosso banco de dados</Typography>
       </Grid>
       <Grid container justifyContent="center" alignItems="center" spacing={2} sx={{ marginTop: 4 }}>
         <Grid item xs={12} sm={6} md={4} lg={2}>
@@ -69,8 +45,9 @@ export default function Dashboard() {
       <Grid container justifyContent="center" alignItems="center" spacing={2} sx={{ marginTop: 8 }}>
       <Grid container item xs={12} md={7} lg={8}>
         <Grid container>
-          <Grid item >
+          <Grid item xs={12} lg={12} sx={{ mb: -2.25 }} textAlign={"center"}>
             <Typography variant="h5">Tabela</Typography>
+            <Typography variant="p">Tabela parcial de 2024</Typography>
           </Grid>
           <Grid item />
         </Grid>
@@ -86,32 +63,78 @@ export default function Dashboard() {
             <ForwardOutlinedIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1, fontSize: 50 }} />
         </Grid>
       </Grid>
-      {/* <Grid item xs={12} md={5} lg={4}>
-        <Grid container>
-          <Grid item>
-            <Typography variant="h5">Analytics Report</Typography>
-          </Grid>
-          <Grid item />
-        </Grid> */}
-        {/* <MainCard sx={{ mt: 2 }} content={false}>
-          <List sx={{ p: 0, '& .MuiListItemButton-root': { py: 2 } }}>
-            <ListItemButton divider>
-              <ListItemText primary="Company Finance Growth" />
-              <Typography variant="h5">+45.14%</Typography>
-            </ListItemButton>
-            <ListItemButton divider>
-              <ListItemText primary="Company Expenses Ratio" />
-              <Typography variant="h5">0.58%</Typography>
-            </ListItemButton>
-            <ListItemButton>
-              <ListItemText primary="Business Risk Cases" />
-              <Typography variant="h5">Low</Typography>
-            </ListItemButton>
-          </List>
-          <ReportAreaChart />
-        </MainCard>
-      </Grid> */}
+      </Grid> 
+
+      <Grid container spacing={2} sx={{ marginBottom: 10, marginTop: 10, justifyContent: "center" }}>
+      <Grid item xs={12} lg={12} sx={{ mb: -2.25 }} textAlign={"center"}>
+        <Typography variant="h5">Estatísticas Gerais</Typography>
+        <Typography variant="p">Campeonato de 2024</Typography>
       </Grid>
-    </Grid>      
+  {/* Primeiro Gráfico */}
+  <Grid item xs={12} md={4} lg={3}>
+    <MainCard sx={{ mt: 2 }} content={false}>
+      <List sx={{ p: 0, '& .MuiListItemButton-root': { py: 2 } }}>
+        <ListItemButton divider>
+          <ListItemText primary="Company Finance Growth" />
+          <Typography variant="h5">+45.14%</Typography>
+        </ListItemButton>
+        <ListItemButton divider>
+          <ListItemText primary="Company Expenses Ratio" />
+          <Typography variant="h5">0.58%</Typography>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemText primary="Business Risk Cases" />
+          <Typography variant="h5">Low</Typography>
+        </ListItemButton>
+      </List>
+      <ReportAreaChart />
+    </MainCard>
+  </Grid>
+
+  {/* Segundo Gráfico */}
+  <Grid item xs={12} md={4} lg={3}>
+    <MainCard sx={{ mt: 2 }} content={false}>
+      <List sx={{ p: 0, '& .MuiListItemButton-root': { py: 2 } }}>
+        <ListItemButton divider>
+          <ListItemText primary="Company Finance Growth" />
+          <Typography variant="h5">+45.14%</Typography>
+        </ListItemButton>
+        <ListItemButton divider>
+          <ListItemText primary="Company Expenses Ratio" />
+          <Typography variant="h5">0.58%</Typography>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemText primary="Business Risk Cases" />
+          <Typography variant="h5">Low</Typography>
+        </ListItemButton>
+      </List>
+      <ReportAreaChart />
+    </MainCard>
+  </Grid>
+
+  {/* Terceiro Gráfico */}
+  <Grid item xs={12} md={4} lg={3}>
+    <MainCard sx={{ mt: 2 }} content={false}>
+      <List sx={{ p: 0, '& .MuiListItemButton-root': { py: 2 } }}>
+        <ListItemButton divider>
+          <ListItemText primary="Company Finance Growth" />
+          <Typography variant="h5">+45.14%</Typography>
+        </ListItemButton>
+        <ListItemButton divider>
+          <ListItemText primary="Company Expenses Ratio" />
+          <Typography variant="h5">0.58%</Typography>
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemText primary="Business Risk Cases" />
+          <Typography variant="h5">Low</Typography>
+        </ListItemButton>
+      </List>
+      <ReportAreaChart />
+    </MainCard>
+  </Grid>
+</Grid>
+</Grid>
+
+
   );
 }

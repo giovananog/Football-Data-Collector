499619822d408e6cc708670f29d872c2aef3e45f
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-10-19 18:25:34 -0300

    feat: start teams page on frontend

---
+++ frontend/thm-fdc/src/components/teams/Teams.jsx
@@ -0,0 +1,21 @@
+import { React } from "react";
+import Header from "../header/Header";
+import { Link } from "@mui/material";
+import AppFooter from '../footer/AppFooter';
+import Dashboard from './Dashboard'
+import ProductCTA from './ProductCTA';
+
+function Teams() {
+
+  return (
+    <div>
+
+      <Header />
+      <ProductCTA />
+      <AppFooter />
+      
+    </div>
+  );
+}
+
+export default Teams;

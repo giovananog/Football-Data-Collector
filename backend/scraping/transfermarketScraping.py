26e505f5687aff0a6b834a8fd9bc04b5ed06c9a2
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-10-27 20:11:33 -0300

    feat: top goalscorers of competitions data scraping added on transfermarket scraping

--- backend/scraping/transfermarketScraping.py
+++ backend/scraping/transfermarketScraping.py
@@ -9,7 +9,86 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 }
 
+def top_goalscorers():
+    try:
+        base_url = "https://www.transfermarkt.com/campeonato-brasileiro-serie-a/torschuetzenliste/wettbewerb/BRA1/plus/1?saison_id=1995&detailpos=&altersklasse=alle"
+        
+        try:
+            with open("top_goalscorers_dict.json", "r") as file:
+                data_dict = json.load(file)
+        except FileNotFoundError:
+            data_dict = {}
+        
+        ano = 1995
+        
+        for _ in range(28):
+            
+            url = base_url.replace(str(1995), str(ano))
+            ano += 1
+            if str(ano) in data_dict:
+                continue
+            
+            print(url)
+                        
+            response = requests.get(url, headers=headers)
+            data = response.text
+            soup = BeautifulSoup(data, 'html.parser')
+            
+            elementos = soup.select('.responsive-table td')
+            img = soup.select('.inline-table img')
+            
+            teams = []
+            team_data = {}
+            clean_elements = []
+            i = 0
+            
+            for e in elementos:
+                if (e.get_text().strip() != "" and e.get_text().strip() != "-"):
+                    clean_elements.append(e)
 
+            for index, elemento in enumerate(clean_elements):
+                text = elemento.get_text().strip()
+        
+                if index % 12 == 0:
+                    if team_data:  
+                        teams.append(team_data)
+                    team_data = {'Table Position': text}  
+                elif index % 12 == 2:
+                    team_data['Name'] = text
+                elif index % 12 == 3:
+                    team_data['Position'] = text
+                elif index % 12 == 4:
+                    team_data['Age'] = text.split('(')[0]
+                elif index % 12 == 5:
+                    team_data['Appearances'] = text
+                elif index % 12 == 6:
+                    team_data['Assists'] = text
+                elif index % 12 == 7:
+                    team_data['Penalty Kicks'] = text
+                elif index % 12 == 8:
+                    team_data['Minutes Played'] = text
+                elif index % 12 == 9:
+                    team_data['Minutes per Goal'] = text
+                elif index % 12 == 10:
+                    team_data['Goals per Match'] = text
+                elif index % 12 == 11:
+                    team_data['Goals'] = text
+                    team_data["Image"] = img[i].get_attribute_list('data-src')[0]
+                    i += 1
+
+                if(i == 5): break
+            if team_data:
+                teams.append(team_data)  
+
+            data_dict[ano] = teams
+
+        with open("top_goalscorers_dict.json", "w") as file:
+            json.dump(data_dict, file, indent=4)
+                    
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+  
+  
 def tables_data():
     
     try:
@@ -450,7 +529,6 @@ def match_data(matches_list):
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
 
-
 def player_data(url, list):
     base_url = 'https://www.transfermarkt.com'
     response = requests.get(url, headers=headers)
@@ -681,6 +759,7 @@ def manager_data(url, list):
     except Exception as e:
         print(f"Ocorreu um erro (manager): {e}")
 
-matches_list = matches_data()
-match_data(matches_list)
+# matches_list = matches_data()
+# match_data(matches_list)
 # tables_data()
+top_goalscorers()

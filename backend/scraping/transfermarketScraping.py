0100509f541f49c44e7e9fd930b61d3822ead14c
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-10-29 20:34:14 -0300

    update stadium scraping on transfermarket

--- backend/scraping/transfermarketScraping.py
+++ backend/scraping/transfermarketScraping.py
@@ -893,26 +893,68 @@ def stadium_data(url, list):
     soup = BeautifulSoup(data, 'html.parser')
     
     stadium_id = url.split('/')[-3]   
-    print('base_url + url') 
+    print(base_url + url) 
     
     try:
-        stadium_info = {}
-        titles = soup.select(f'.profilheader th')
-        contents = soup.select(f'.profilheader td')
         img = soup.select('.datenfakten-wappen img')
-
-                    
-        for title, content in zip(titles, contents):
-            title_text = title.get_text().strip().split(':')[0].split(' ')[0]
-            content_text = content.get_text().strip()
-            
-            stadium_info[title_text] = content_text
         
         try:
             stadium_info["Image"] = img[0].get_attribute_list('src')[0]
         except:
             pass
         
+        stadium_info = {
+            "name": None,
+            "capacity": None,
+            "boxes": None,
+            "box_seats": None,
+            "built": None,
+            "formerly": None,
+            "undersoil_heating": None,
+            "running_track": None,
+            "surface": None,
+            "pitch_size": None,
+            "telefone": None,
+            "address": []
+        }
+
+        full_address = []
+        for row in soup.select('.profilheader tr'):
+            th = row.find('th')
+            td = row.find('td')
+            
+            if th and td:
+                label = th.get_text(strip=True)
+                content_text = td.get_text(strip=True)
+
+                if label == "Name of stadium:":
+                    stadium_info["name"] = content_text
+                elif label == "Total capacity:":
+                    stadium_info["capacity"] = content_text
+                elif label == "Boxes:":
+                    stadium_info["boxes"] = content_text
+                elif label == "Box seats:":
+                    stadium_info["box_seats"] = content_text
+                elif label == "Built:":
+                    stadium_info["built"] = content_text
+                elif label == "Formerly:":
+                    stadium_info["formerly"] = content_text
+                elif label == "Undersoil heating:":
+                    stadium_info["undersoil_heating"] = content_text
+                elif label == "Running track:":
+                    stadium_info["running_track"] = content_text
+                elif label == "Surface:":
+                    stadium_info["surface"] = content_text
+                elif label == "Pitch size:":
+                    stadium_info["pitch_size"] = content_text
+                elif label == "Tel:":
+                    stadium_info["tel"] = content_text
+                if label == "Address:" or label == "":
+                    if(not content_text.startswith('(') and not content_text.startswith('including')):
+                        full_address.append(content_text)
+        
+        complete_address = "\n".join(full_address)
+        stadium_info["address"] = complete_address
         list[stadium_id] = stadium_info
         return stadium_id
         

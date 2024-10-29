fabfc8fa74a7055f6b4e20e1059d0e8858374854
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-10-29 17:28:49 -0300

    feat: update app routes on fronend

--- frontend/thm-fdc/src/components/App.jsx
+++ frontend/thm-fdc/src/components/App.jsx
@@ -9,8 +9,12 @@ import 'typeface-poppins';
 import Home from "./home/Home";
 import Teams from "./teams/Teams";
 import Team from "./team/Team";
+import Player from "./player/Player";
 import Matches from "./matches/Matches";
 import Match from "./match/Match";
+import Referee from "./referee/Referee";
+import Manager from "./manager/Manager";
+import Stadium from "./stadium/Stadium";
 
 function App() {
   return (
@@ -23,6 +27,10 @@ function App() {
           <Route path="/campeonatos/:times/:time" element={<Team/>}></Route>
           <Route path="/campeonatos/:id/partidas" element={<Matches/>}></Route>
           <Route path="/campeonatos/:id/partidas/:id" element={<Match/>}></Route>
+          <Route path="/campeonatos/:id/jogadores/:a" element={<Player/>}></Route>
+          <Route path="/juizes/:a" element={<Referee/>}></Route>
+          <Route path="/tecnicos/:a" element={<Manager/>}></Route>
+          <Route path="/estadios/:a" element={<Stadium/>}></Route>
           
         </Routes>
 

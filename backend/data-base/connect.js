0c4f9846a6bd18f544e71f199d56f0bf558b4da9
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2024-11-02 00:06:30 -0300

    feat: add data base connection file

---
+++ backend/data-base/connect.js
@@ -0,0 +1,12 @@
+import pkg from 'pg'; 
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: 'postgres',      
+    host: 'localhost',        
+    database: 'FootballData',    
+    password: process.env.DB_PASS,    
+    port: 5432,               
+});
+
+export default pool;
